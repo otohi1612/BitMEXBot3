dd2d54978a22cd93099e218b7a272272535d5a9a
otohi1612 <otohi1612@users.noreply.github.com>
2018-04-19 20:24:37 +0900

    First commit.

---
+++ BitMEXBot3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitMEXBot3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
